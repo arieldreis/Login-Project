5069b915debf1e8674300ffd3dfbc68266311239
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-09-30 11:27:47 -0300

    Refactor image div structure and improve input validation logic in login script

--- project/script.js
+++ project/script.js
@@ -3,17 +3,28 @@ const userid = document.getElementById('idemail');
 const passwordId = document.getElementById('idpassword');
 const verficacao = document.getElementById('verficacao');
 const buttonForm = document.getElementById('buttonForm');
+const img =  document.getElementById('img');
 
-// Inputs verficados
-const user = userid.value;
-const password = passwordId.value;
 
 buttonForm.addEventListener('click', (e) => {
     e.preventDefault();
+    // Inputs verficados
+    const user = userid.value;
+    const password = passwordId.value;
     if(user === "" || password === ""){
         verficacao.innerHTML = "Por favor, preencha todos os campos.";
         verficacao.style.color = "red";
         verficacao.style.fontWeight = "bolder";
         verficacao.style.textAlign = "center";
+        img.style.height = "71%";
+    }else{
+        verficacao.innerHTML = "Login realizado com sucesso! 🎉";
+        verficacao.style.color = "green";
+        verficacao.style.fontWeight = "bolder";
+        verficacao.style.textAlign = "center";
+        img.style.height = "71%";
     }
+
+    userid.value = "";
+    passwordId.value = "";
 });

31b14f99d21751374ef725ddc79415cfbdf6b204
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-09-29 23:08:17 -0300

    Add form validation and feedback for login inputs

--- project/script.js
+++ project/script.js
@@ -1,1 +1,19 @@
 // Code 
+const userid = document.getElementById('idemail');
+const passwordId = document.getElementById('idpassword');
+const verficacao = document.getElementById('verficacao');
+const buttonForm = document.getElementById('buttonForm');
+
+// Inputs verficados
+const user = userid.value;
+const password = passwordId.value;
+
+buttonForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(user === "" || password === ""){
+        verficacao.innerHTML = "Por favor, preencha todos os campos.";
+        verficacao.style.color = "red";
+        verficacao.style.fontWeight = "bolder";
+        verficacao.style.textAlign = "center";
+    }
+});
